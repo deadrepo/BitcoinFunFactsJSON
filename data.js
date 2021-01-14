b7fc16fc366f336d015435df5659f79eee5fa962
h4x00r <50273964+4zer0day@users.noreply.github.com>
2021-01-14 22:11:32 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,22 @@
+exports.fruitFacts = {
+  blockchain: [
+    'Bitcoin is claimed to have been created by a mysterious person, or a group of people, referred to as Satoshi Nakamoto. The name, however, is a pseudonym to keep the actual person behind it off the grid.',
+    'Satoshi Nakamoto in Japanese, the name roughly translates into:\nSatoshi: clear-thinking, quick-witted, wise \nNaka: inside, or relationship \nMoto: the foundation',
+    'Some conspiracy theorists even believe that it was created by 4 tech giants and that the name was derived from the first two letters of the name of the first company, and the first 4 letters of the name of the remaining three companies.\nThey include\nSamsung: SA\nToshiba: TOSHI\nNakamichi: NAKA\nMotorola: MOTO,\nto give the name: SATOSHI NAKAMOTO',
+    'Bitcoin is decentralized and not controlled by a single entity, some individuals have argued otherwise. This is because 50% of the hashrate—which refers to the amount of computing power needed to mine it—is controlled by 4 major entities, which thus creates a centralized weak point and undermines the operating principle of cryptocurrency. The entities include:\n· BTC.com\n· AntPool\n· ConnectBTC\n· BTC.TOP\nBased on this alone, it is not enough to conclude that Bitcoin is centralized, as other competitors are not relenting.',
+    'The first ever Bitcoin transaction was the purchase of 2 pizzas, with 10,000 bitcoin by Hanyecz on May 22nd, 2010, a situation that will most likely be an, “had I known” case, owing to the current value of one bitcoin. Today, this transaction is tagged as the most expensive food order in history.',
+    'The date Hanyecz made his purchase , May 22nd, is now termed, “Bitcoin Pizza Day” and is celebrated by cryptocurrency enthusiasts worldwide.',
+    'There are some weird things that can be purchased with bitcoin, since the seller accepts it as a mode of payment. Some of them include: a goat, pocket cannon, a webcam show and even a trip to outer space',
+    'Bitcoin is built on a distributed, or peer-to-peer, network, so related activities are authenticated and validated by the mass collaboration of individuals within the ecosystem (miners, core developers, investors, merchants etc), and it is powered by collective interests',
+    'To carry out a transaction with bitcoin, you have to generate two keys, which are referred to as public and private keys. Both keys are large integer and are usually represented in figures and letters, and without them, transactions can’t be carried out',
+    'The creator(s) of Bitcoin owns around 21 1 million of the total that will ever be in existence.',
+    'The estimated year that the last bitcoin will be mined is 2140, based on the Bitcoin block having a dividing frequency of four years',
+    'There are lots of predictions with regards to Bitcoin’s death. Totalling 306, they usually occur as a result of heavy crashes in its valuation. Interestingly, you can even generate your own online Bitcoin obituary these days',
+    'The domain name "bitcoin.org" was registered on 18 August 2008.On 31 October 2008, a link to a paper authored by Satoshi Nakamoto titled Bitcoin: A Peer-to-Peer Electronic Cash System was posted to a cryptography mailing list. Nakamoto implemented the bitcoin software as open-source code and released it in January 2009',
+    '4zer0day satoshisyurei'
+  ]
+};
+
+exports.fruits = [
+ 'blockchain'
+];
